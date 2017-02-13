d512fab42065cfdc6d486f66ddc6d7f0a99a4b53
thenryci1996 <thenry702@gmail.com>
2017-02-13 15:32:52 +0100

    First work
    Work on the splash screen activity
    Add Login activity for Juan to move his work

---
+++ ForeignFriend/app/src/main/java/com/group11/kth/foreignfriend/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.group11.kth.foreignfriend;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // create a thread for the splash screen
+        Thread splashScreen = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),
+                            MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        splashScreen.start();
+
+    }
+}
